2a27e154ecbf149d776457b4673c12042f1c0983
Samu Kallio <samu@samukallio.net>
2024-04-11 17:34:16 +0300

    Create GLFW window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+#include <GLFW/glfw3.h>
+
+int const WINDOW_WIDTH = 1024;
+int const WINDOW_HEIGHT = 768;
+
 int main()
 {
-	printf("Hello world\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Path Tracer", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
