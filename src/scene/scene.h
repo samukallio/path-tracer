fa177dbb43133a4fc053a359ba8e4444cf89ab83
Samu Kallio <samu@samukallio.net>
2024-06-16 15:20:22 +0300

    Fix missing files due to .gitignore

---
+++ src/scene/scene.h
@@ -0,0 +1,311 @@
+#pragma once
+
+#include "renderer/common.h"
+
+struct texture
+{
+    std::string                     Name                    = "New Texture";
+    texture_type                    Type                    = TEXTURE_TYPE_RAW;
+    bool                            EnableNearestFiltering  = false;
+
+    uint32_t                        Width                   = 0;
+    uint32_t                        Height                  = 0;
+    glm::vec4 const*                Pixels                  = nullptr;
+
+    uint32_t                        PackedTextureIndex      = 0;
+};
+
+struct material
+{
+    std::string                     Name                    = "New Material";
+    uint32_t                        Flags                   = 0;
+
+    float                           Opacity                 = 1.0f;
+
+    float                           BaseWeight              = 1.0f;
+    glm::vec3                       BaseColor               = glm::vec3(1, 1, 1);
+    texture*                        BaseColorTexture        = nullptr;
+    float                           BaseMetalness           = 0.0f;
+    float                           BaseDiffuseRoughness    = 0.0f;
+
+    float                           SpecularWeight          = 1.0f;
+    glm::vec3                       SpecularColor           = glm::vec3(1, 1, 1);
+    float                           SpecularRoughness       = 0.3f;
+    texture*                        SpecularRoughnessTexture = nullptr;
+    float                           SpecularRoughnessAnisotropy = 0.0f;
+    float                           SpecularIOR             = 1.5f;
+
+    float                           TransmissionWeight      = 0.0f;
+    glm::vec3                       TransmissionColor       = glm::vec3(1, 1, 1);
+    float                           TransmissionDepth       = 0.0f;
+    glm::vec3                       TransmissionScatter     = glm::vec3(0, 0, 0);
+    float                           TransmissionScatterAnisotropy = 0.0f;
+    float                           TransmissionDispersionScale = 0.0f;
+    float                           TransmissionDispersionAbbeNumber = 20.0f;
+
+    float                           CoatWeight              = 0.0f;
+    glm::vec3                       CoatColor               = glm::vec3(1, 1, 1);
+    float                           CoatRoughness           = 0.0f;
+    float                           CoatRoughnessAnisotropy = 0.0f;
+    float                           CoatIOR                 = 1.6f;
+    float                           CoatDarkening           = 1.0f;
+
+    float                           EmissionLuminance       = 0.0f;
+    glm::vec3                       EmissionColor           = glm::vec3(0, 0, 0);
+    texture*                        EmissionColorTexture    = nullptr;
+
+    int                             LayerBounceLimit        = 16;
+
+    uint32_t                        PackedMaterialIndex     = 0;
+};
+
+struct mesh_face
+{
+    glm::vec3                       Vertices[3];
+    glm::vec3                       Centroid;
+    glm::vec3                       Normals[3];
+    glm::vec2                       UVs[3];
+    uint32_t                        MaterialIndex;
+};
+
+struct mesh_node
+{
+    bounds                          Bounds;
+    uint32_t                        FaceBeginIndex;
+    uint32_t                        FaceEndIndex;
+    uint32_t                        ChildNodeIndex;
+};
+
+struct mesh
+{
+    std::string                     Name;
+    std::vector<mesh_face>          Faces;
+    std::vector<mesh_node>          Nodes;
+    uint32_t                        Depth;
+    uint32_t                        PackedRootNodeIndex;
+    std::vector<material*>          Materials;
+};
+
+enum entity_type
+{
+    ENTITY_TYPE_ROOT                = 0,
+    ENTITY_TYPE_CONTAINER           = 1,
+    ENTITY_TYPE_CAMERA              = 2,
+    ENTITY_TYPE_MESH_INSTANCE       = 3,
+    ENTITY_TYPE_PLANE               = 4,
+    ENTITY_TYPE_SPHERE              = 5,
+    ENTITY_TYPE_CUBE                = 6,
+
+    ENTITY_TYPE__COUNT              = 7,
+};
+
+struct entity
+{
+    std::string                     Name                    = "Entity";
+    entity_type                     Type                    = ENTITY_TYPE_ROOT;
+    bool                            Active                  = true;
+    transform                       Transform               = {};
+    entity*                         Parent                  = nullptr;
+    std::vector<entity*>            Children                = {};
+    uint32_t                        PackedShapeIndex        = SHAPE_INDEX_NONE;
+
+    virtual ~entity() {}
+};
+
+struct root : entity
+{
+    float                           ScatterRate             = 0.0f;
+    float                           SkyboxBrightness        = 1.0f;
+    texture*                        SkyboxTexture           = nullptr;
+
+    root() { Type = ENTITY_TYPE_ROOT; }
+};
+
+struct container : entity
+{
+    container() { Type = ENTITY_TYPE_CONTAINER; }
+};
+
+struct camera_pinhole
+{
+    float                           FieldOfViewInDegrees    = 90.000f;
+    float                           ApertureDiameterInMM    = 0.0f;
+};
+
+struct camera_thin_lens
+{
+    glm::vec2                       SensorSizeInMM          = { 32.0f, 18.0f };
+    float                           FocalLengthInMM         = 20.0f;
+    float                           ApertureDiameterInMM    = 10.0f;
+    float                           FocusDistance           = 1.0f;
+};
+
+struct camera : entity
+{
+    render_mode                     RenderMode              = RENDER_MODE_PATH_TRACE;
+    uint32_t                        RenderFlags             = 0;
+    uint32_t                        RenderBounceLimit       = 5;
+    uint32_t                        RenderMeshComplexityScale = 32;
+    uint32_t                        RenderSceneComplexityScale = 32;
+    uint32_t                        RenderSampleBlockSizeLog2 = 0;
+    float                           RenderTerminationProbability = 0.0f;
+
+    float                           Brightness              = 1.0f;
+    tone_mapping_mode               ToneMappingMode         = TONE_MAPPING_MODE_CLAMP;
+    float                           ToneMappingWhiteLevel   = 1.0f;
+
+    camera_model                    CameraModel             = CAMERA_MODEL_PINHOLE;
+    camera_pinhole                  Pinhole                 = {};
+    camera_thin_lens                ThinLens                = {};
+
+    glm::vec3                       Velocity                = { 0, 0, 0 };
+
+    camera() { ((entity*)this)->Type = ENTITY_TYPE_CAMERA; }
+};
+
+struct mesh_instance : entity
+{
+    mesh*                           Mesh = nullptr;
+
+    mesh_instance() { Type = ENTITY_TYPE_MESH_INSTANCE; }
+};
+
+struct plane : entity
+{
+    material*                       Material = nullptr;
+
+    plane() { Type = ENTITY_TYPE_PLANE; }
+};
+
+struct sphere : entity
+{
+    material*                       Material = nullptr;
+
+    sphere() { Type = ENTITY_TYPE_SPHERE; }
+};
+
+struct cube : entity
+{
+    material*                       Material = nullptr;
+
+    cube() { Type = ENTITY_TYPE_CUBE; }
+};
+
+struct prefab
+{
+    entity*                         Entity                  = nullptr;
+};
+
+struct parametric_spectrum_table
+{
+    static constexpr int SCALE_BINS = 64;
+    static constexpr int COLOR_BINS = 64;
+
+    glm::vec3 Coefficients[3][SCALE_BINS][COLOR_BINS][COLOR_BINS];
+};
+
+enum scene_dirty_flag
+{
+    SCENE_DIRTY_TEXTURES            = 1 << 0,
+    SCENE_DIRTY_MATERIALS           = 1 << 1,
+    SCENE_DIRTY_SHAPES              = 1 << 2,
+    SCENE_DIRTY_MESHES              = 1 << 3,
+    SCENE_DIRTY_CAMERAS             = 1 << 4,
+    SCENE_DIRTY_ALL                 = 0xFFFFFFFF,
+};
+
+struct scene
+{
+    // Source description of the scene entities and assets.
+    root                                Root;
+    std::vector<mesh*>                  Meshes;
+    std::vector<material*>              Materials;
+    std::vector<texture*>               Textures;
+    std::vector<prefab*>                Prefabs;
+    parametric_spectrum_table*          RGBSpectrumTable;
+
+    // Data derived from the source data, packed and optimized
+    // for rendering on the GPU.  Generated by PackSceneData().
+    std::vector<image>                  Images;
+    std::vector<packed_texture>         TexturePack;
+    std::vector<packed_shape>           ShapePack;
+    std::vector<packed_shape_node>      ShapeNodePack;
+    std::vector<packed_material>        MaterialPack;
+    std::vector<packed_mesh_face>       MeshFacePack;
+    std::vector<packed_mesh_face_extra> MeshFaceExtraPack;
+    std::vector<packed_mesh_node>       MeshNodePack;
+
+    glm::mat3                           SkyboxDistributionFrame;
+    float                               SkyboxDistributionConcentration;
+
+    // Flags that track which portion of the source description has
+    // changed relative to the packed data since the last call to
+    // PackSceneData().
+    uint32_t                            DirtyFlags;
+};
+
+struct load_model_options
+{
+    char const*     Name                        = nullptr;
+    material*       DefaultMaterial             = nullptr;
+    std::string     DirectoryPath               = ".";
+    glm::mat4       VertexTransform             = glm::mat4(1);
+    glm::mat4       NormalTransform             = glm::mat4(1);
+    glm::mat3       TextureCoordinateTransform  = glm::mat3(1);
+    bool            MergeIntoSingleMesh         = false;
+};
+
+inline uint32_t GetPackedTextureIndex(texture* Texture)
+{
+    if (!Texture) return TEXTURE_INDEX_NONE;
+    return Texture->PackedTextureIndex;
+}
+
+inline uint32_t GetPackedMaterialIndex(material* Material)
+{
+    if (!Material) return 0;
+    return Material->PackedMaterialIndex;
+}
+
+// --- scene.cpp --------------------------------------------------------------
+
+char const* EntityTypeName(entity_type Type);
+
+entity*     CreateEntityRaw(entity_type Type);
+entity*     CreateEntity(scene* Scene, entity_type Type, entity* Parent = nullptr);
+entity*     CreateEntity(scene* Scene, entity* Source, entity* Parent = nullptr);
+entity*     CreateEntity(scene* Scene, prefab* Prefab, entity* Parent = nullptr);
+void        DestroyEntity(scene* Scene, entity* Entity);
+
+material*   CreateMaterial(scene* Scene, char const* Name);
+void        DestroyMaterial(scene* Scene, material* Material);
+
+texture*    CreateCheckerTexture(scene* Scene, char const* Name, texture_type Type, glm::vec4 const& ColorA, glm::vec4 const& ColorB);
+texture*    LoadTexture(scene* Scene, char const* Path, texture_type Type, char const* Name = nullptr);
+void        DestroyTexture(scene* Scene, texture* Texture);
+
+void        DestroyMesh(scene* Scene, mesh* Mesh);
+
+prefab*     LoadModelAsPrefab(scene* Scene, char const* Path, load_model_options* Options = nullptr);
+void        DestroyPrefab(scene* Scene, prefab* Prefab);
+
+scene*      CreateScene();
+scene*      LoadScene(char const* Path);
+void        SaveScene(char const* Path, scene* Scene);
+void        DestroyScene(scene* Scene);
+
+uint32_t    PackSceneData(scene* Scene);
+
+entity*     FindEntityByPackedShapeIndex(scene* Scene, uint32_t PackedShapeIndex);
+
+// --- trace.cpp --------------------------------------------------------------
+
+bool        Trace(scene* Scene, ray const& Ray, hit& Hit);
+
+// --- spectrum.cpp -----------------------------------------------------------
+
+void        BuildParametricSpectrumTableForSRGB(parametric_spectrum_table* Table);
+bool        SaveParametricSpectrumTable(parametric_spectrum_table const* Table, char const* Path);
+bool        LoadParametricSpectrumTable(parametric_spectrum_table* Table, char const* Path);
+glm::vec3   GetParametricSpectrumCoefficients(parametric_spectrum_table const* Table, glm::vec3 const& Color);
+float       SampleParametricSpectrum(glm::vec3 const& Beta, float Lambda);
---
+++ src/scene/trace.cpp
@@ -0,0 +1,200 @@
+#include "path-tracer.h"
+#include "scene/scene.h"
+
+static void IntersectMeshFace(scene* Scene, ray Ray, uint32_t MeshFaceIndex, hit& Hit)
+{
+    packed_mesh_face Face = Scene->MeshFacePack[MeshFaceIndex];
+
+    float R = glm::dot(Face.Plane.xyz(), Ray.Vector);
+    if (R > -EPSILON && R < +EPSILON) return;
+
+    float T = -(glm::dot(Face.Plane.xyz(), Ray.Origin) + Face.Plane.w) / R;
+    if (T < 0 || T > Hit.Time) return;
+
+    glm::vec3 V = Ray.Origin + Ray.Vector * T - Face.Position;
+    float Beta = glm::dot(glm::vec3(Face.Base1), V);
+    if (Beta < 0 || Beta > 1) return;
+    float Gamma = glm::dot(glm::vec3(Face.Base2), V);
+    if (Gamma < 0 || Beta + Gamma > 1) return;
+
+    Hit.Time = T;
+    Hit.ShapeType = SHAPE_TYPE_MESH_INSTANCE;
+    Hit.ShapeIndex = SHAPE_INDEX_NONE;
+    Hit.PrimitiveIndex = MeshFaceIndex;
+    Hit.PrimitiveCoordinates = glm::vec3(1 - Beta - Gamma, Beta, Gamma);
+}
+
+static float IntersectMeshNodeBounds(ray Ray, float Reach, packed_mesh_node const& Node)
+{
+    // Compute ray time to the axis-aligned planes at the node bounding
+    // box minimum and maximum corners.
+    glm::vec3 Minimum = (Node.Minimum - Ray.Origin) / Ray.Vector;
+    glm::vec3 Maximum = (Node.Maximum - Ray.Origin) / Ray.Vector;
+
+    // For each coordinate axis, sort out which of the two coordinate
+    // planes (at bounding box min/max points) comes earlier in time and
+    // which one comes later.
+    glm::vec3 Earlier = min(Minimum, Maximum);
+    glm::vec3 Later = max(Minimum, Maximum);
+
+    // Compute the ray entry and exit times.  The ray enters the box when
+    // it has crossed all of the entry planes, so we take the maximum.
+    // Likewise, the ray has exit the box when it has exit at least one
+    // of the exit planes, so we take the minimum.
+    float Entry = glm::max(glm::max(Earlier.x, Earlier.y), Earlier.z);
+    float Exit = glm::min(glm::min(Later.x, Later.y), Later.z);
+
+    // If the exit time is greater than the entry time, then the ray has
+    // missed the box altogether.
+    if (Exit < Entry) return INFINITY;
+
+    // If the exit time is less than 0, then the box is behind the eye.
+    if (Exit <= 0) return INFINITY;
+
+    // If the entry time is greater than previous hit time, then the box
+    // is occluded.
+    if (Entry >= Reach) return INFINITY;
+
+    return Entry;
+}
+
+static void IntersectMesh(scene* Scene, ray const& Ray, uint32_t RootNodeIndex, hit& Hit)
+{
+    uint32_t Stack[32];
+    uint32_t Depth = 0;
+
+    packed_mesh_node Node = Scene->MeshNodePack[RootNodeIndex];
+
+    while (true) {
+        // Leaf node or internal?
+        if (Node.FaceEndIndex > 0) {
+            // Leaf node, trace all geometry within.
+            for (uint32_t FaceIndex = Node.FaceBeginOrNodeIndex; FaceIndex < Node.FaceEndIndex; FaceIndex++)
+                IntersectMeshFace(Scene, Ray, FaceIndex, Hit);
+        }
+        else {
+            // Internal node.
+            // Load the first subnode as the node to be processed next.
+            uint32_t Index = Node.FaceBeginOrNodeIndex;
+            Node = Scene->MeshNodePack[Index];
+            float Time = IntersectMeshNodeBounds(Ray, Hit.Time, Node);
+
+            // Also load the second subnode to see if it is closer.
+            uint32_t IndexB = Index + 1;
+            packed_mesh_node NodeB = Scene->MeshNodePack[IndexB];
+            float TimeB = IntersectMeshNodeBounds(Ray, Hit.Time, NodeB);
+
+            // If the second subnode is strictly closer than the first one,
+            // then it was definitely hit, so process it next.
+            if (Time > TimeB) {
+                // If the first subnode was also hit, then set it aside for later.
+                if (Time < INFINITY) {
+                    assert(Depth < std::size(Stack));
+                    Stack[Depth++] = Index;
+                }
+                Node = NodeB;
+                continue;
+            }
+
+            // The first subnode is at least as close as the second one.
+            // If the second subnode was hit, then both of them were,
+            // and we should set the second one aside for later.
+            if (TimeB < INFINITY) {
+                assert(Depth < std::size(Stack));
+                Stack[Depth++] = IndexB;
+                continue;
+            }
+
+            // The first subnode is at least as close as the second one,
+            // and the second subnode was not hit.  If the first one was
+            // hit, then process it next.
+            if (Time < INFINITY) continue;
+        }
+
+        // Just processed a leaf node or an internal node with no intersecting
+        // subnodes.  If the stack is also empty, then we are done.
+        if (Depth == 0) break;
+
+        // Pull a node from the stack.
+        Node = Scene->MeshNodePack[Stack[--Depth]];
+    }
+}
+
+static void IntersectShape(scene* Scene, ray const& WorldRay, uint32_t ShapeIndex, hit& Hit)
+{
+    packed_shape Shape = Scene->ShapePack[ShapeIndex];
+
+    ray Ray = TransformRay(WorldRay, Shape.Transform.From);
+
+    if (Shape.Type == SHAPE_TYPE_MESH_INSTANCE) {
+        IntersectMesh(Scene, Ray, Shape.MeshRootNodeIndex, Hit);
+        if (Hit.ShapeIndex == SHAPE_INDEX_NONE)
+            Hit.ShapeIndex = ShapeIndex;
+    }
+
+    if (Shape.Type == SHAPE_TYPE_PLANE) {
+        float T = -Ray.Origin.z / Ray.Vector.z;
+        if (T < 0 || T > Hit.Time) return;
+
+        Hit.Time = T;
+        Hit.ShapeType = SHAPE_TYPE_PLANE;
+        Hit.ShapeIndex = ShapeIndex;
+        Hit.PrimitiveIndex = 0;
+        Hit.PrimitiveCoordinates = glm::vec3(glm::fract(Ray.Origin.xy() + Ray.Vector.xy() * T), 0);
+    }
+
+    if (Shape.Type == SHAPE_TYPE_SPHERE) {
+        float V = glm::dot(Ray.Vector, Ray.Vector);
+        float P = glm::dot(Ray.Origin, Ray.Vector);
+        float Q = glm::dot(Ray.Origin, Ray.Origin) - 1.0f;
+        float D2 = P * P - Q * V;
+        if (D2 < 0) return;
+
+        float D = glm::sqrt(D2);
+        if (D < P) return;
+
+        float S0 = -P - D;
+        float S1 = -P + D;
+        float S = S0 < 0 ? S1 : S0;
+        if (S < 0 || S > V * Hit.Time) return;
+
+        Hit.Time = S / V;
+        Hit.ShapeType = SHAPE_TYPE_SPHERE;
+        Hit.ShapeIndex = ShapeIndex;
+        Hit.PrimitiveIndex = 0;
+        Hit.PrimitiveCoordinates = Ray.Origin + Ray.Vector * Hit.Time;
+    }
+
+    if (Shape.Type == SHAPE_TYPE_CUBE) {
+        glm::vec3 Minimum = (glm::vec3(-1,-1,-1) - Ray.Origin) / Ray.Vector;
+        glm::vec3 Maximum = (glm::vec3(+1,+1,+1) - Ray.Origin) / Ray.Vector;
+        glm::vec3 Earlier = min(Minimum, Maximum);
+        glm::vec3 Later = max(Minimum, Maximum);
+        float T0 = glm::max(glm::max(Earlier.x, Earlier.y), Earlier.z);
+        float T1 = glm::min(glm::min(Later.x, Later.y), Later.z);
+        if (T1 < T0) return;
+        if (T1 <= 0) return;
+        if (T0 >= Hit.Time) return;
+
+        float T = T0 < 0 ? T1 : T0;
+
+        Hit.Time = T;
+        Hit.ShapeType = SHAPE_TYPE_CUBE;
+        Hit.ShapeIndex = ShapeIndex;
+        Hit.PrimitiveIndex = 0;
+        Hit.PrimitiveCoordinates = Ray.Origin + Ray.Vector * T;
+    }
+}
+
+static void Intersect(scene* Scene, ray const& WorldRay, hit& Hit)
+{
+    for (uint32_t ShapeIndex = 0; ShapeIndex < Scene->ShapePack.size(); ShapeIndex++)
+        IntersectShape(Scene, WorldRay, ShapeIndex, Hit);
+}
+
+bool Trace(scene* Scene, ray const& Ray, hit& Hit)
+{
+    Hit.Time = INFINITY;
+    Intersect(Scene, Ray, Hit);
+    return Hit.Time < INFINITY;
+}
